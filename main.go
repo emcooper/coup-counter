3c3fc3ddf05dcafb48ad5239f130370798beb3c9
Ellen Cooper <ellencooper74@gmail.com>
2018-09-02 18:06:01 -0600

    adds db connection

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
@@ -28,8 +29,9 @@ func CreateGame(w http.ResponseWriter, r *http.Request) {}
 
 // our main function
 func main() {
-	connString := "dbname=coup_counter_development sslmode=disable"
-	db, err := sql.Open("postgres", connString)
+	// connString := "dbname=coup_counter_development sslmode=disable"
+	// db, err := sql.Open("postgres", connString)
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 
 	if err != nil {
 		panic(err)
@@ -43,7 +45,7 @@ func main() {
 	InitStore(&dbStore{db: db})
 	router := mux.NewRouter()
 
-	router.HandleFunc("/", GetGames).Methods("GET")
+	router.HandleFunc("/games", GetGames).Methods("GET")
 	router.HandleFunc("/games", CreateGame).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
