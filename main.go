65d7954659bec4a8ff9b8efe85a0dfc3aafadec5
Ellen Cooper <emcooper@choozle.com>
2018-11-22 18:47:57 -0800

    fixes cors

--- main.go
+++ main.go
@@ -141,5 +141,5 @@ func main() {
 
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Origin", "Content-Type")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
 }
